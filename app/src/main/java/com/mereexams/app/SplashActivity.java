dc4edae1b1d7e1804520533ec48930ef95c57285
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-09 14:51:02 +0530

    Implemented splash screen

---
+++ app/src/main/java/com/mereexams/app/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.mereexams.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bilal on 09-Jun-17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
